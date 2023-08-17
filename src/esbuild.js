36279f78322ce8e4b7b80212c113657001feaa18
=Xavier <github@tttp.eu>
2023-08-17 07:19:56 +0200

    add esbuild

---
+++ src/esbuild.js
@@ -0,0 +1,24 @@
+//const esbuild = require('esbuild');
+import esbuild from 'esbuild';
+
+esbuild
+    .build({
+        entryPoints: ['src/dxid.js'],
+        outfile: 'dist/index.cjs',
+        bundle: true,
+        format: 'cjs',
+        minify: false,
+        platform: 'node'
+    })
+    .catch(() => process.exit(1));
+
+esbuild
+    .build({
+        entryPoints: ['src/dxid.js'],
+        outfile: 'dist/index.mjs',
+        format: 'esm',
+        bundle: true,
+        minify: false,
+        platform: 'node'
+    })
+    .catch(() => process.exit(1));
